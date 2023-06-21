077aa602e9d2a2ee10fbc9f2570a0b3c5b9b3e23
User <ol.shink87@gmail.com>
2023-06-21 13:43:11 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+    }
+    public static void task1(){
+    }
+    public static void task2(){
+    }
+    public static void task3(){
+    }
+}
